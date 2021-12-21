2b57b1ac1098cb81ce6360c66ce1388483435786
DomantasM <94314249+DomantasM@users.noreply.github.com>
2021-12-21 14:40:41 -0800

    Initial commit

---
+++ app/src/main/java/com/example/mitp_pirmas/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mitp_pirmas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mButton;
+    private Button mButton2;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.textView1);
+        mButton = findViewById(R.id.button_send);
+        mButton.setOnClickListener(this);
+        mButton2 = findViewById(R.id.button_send2);
+        mButton2.setVisibility(View.INVISIBLE);
+        mButton2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.button_send:
+                tv.setText("Hello world!");
+                mButton2.setVisibility(View.VISIBLE);
+            case R.id.button_send2:
+
+        }
+    }
+}
